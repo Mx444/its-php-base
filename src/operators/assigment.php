b5efd80bd178c8fbc40c9ab71ba22b0baee6d54f
Mx444 <mybbtester@gmail.com>
2025-01-15 09:13:48 +0100

    operators

---
+++ src/operators/assigment.php
@@ -0,0 +1,23 @@
+<?php
+
+$a = 10;
+$b = 20;
+
+$c = $a + $b;
+echo "Sum of $a and $b is $c \n";
+
+$c += $a;
+echo "Add AND assigment operation result : $c \n";
+
+$c -= $a;
+echo "Subtract AND assigment operation result : $c \n";
+
+$c *= $a;
+echo "Multiply AND assigment operation result : $c \n";
+
+$c /= $a;
+echo "Divide AND assigment operation result : $c \n";
+
+$c %= $a;
+echo "Modulus AND assigment operation result : $c \n";
+
