0edc5c9144e722f86667079daf6029087b28a439
Mx444 <mybbtester@gmail.com>
2025-01-15 09:20:47 +0100

    operators

---
+++ src/operators/comparasion.php
@@ -0,0 +1,41 @@
+<?php
+
+$a = 10;
+$b = 20;
+
+if($a == $b) {
+    echo "Both numbers are equal \n";
+} else {
+    echo "Numbers are not equal \n";
+}
+
+if($a != $b) {
+    echo "Numbers are not equal \n";
+} else {
+    echo "Both numbers are equal \n";
+}
+
+if($a > $b) {
+    echo "Greater number is $a \n";
+} else {
+    echo "Greater number is $b \n";
+}
+
+if($a < $b) {
+    echo "Smaller number is $a \n";
+} else {
+    echo "Smaller number is $b \n";
+}
+
+if($a >= $b) {
+    echo "Greater number is $a \n";
+} else {
+    echo "Greater number is $b \n";
+}
+
+if($a <= $b) {
+    echo "Smaller number is $a \n";
+} else {
+    echo "Smaller number is $b \n";
+}
+
