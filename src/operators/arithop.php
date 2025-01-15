425945fc3f4bca68fdb6f084b7f64cea2d91dca2
Mx444 <mybbtester@gmail.com>
2025-01-15 09:05:25 +0100

    operators

---
+++ src/operators/arithop.php
@@ -0,0 +1,26 @@
+<?php
+
+$a= 10;
+$b= 20;
+
+$c= $a + $b;
+echo "Sum of $a and $b is $c \n";
+
+$c = $a - $b;
+echo "Difference of $a and $b is $c \n";
+
+$c = $a * $b;
+echo "Product of $a and $b is $c \n";
+
+$c = $a / $b;
+echo "Division of $a and $b is $c \n";
+
+$c = $a % $b;
+echo "Modulus of $a and $b is $c \n";
+
+$c = $a++;
+echo "Increment of $a is $c \n";
+
+$c = $a--;
+echo "Decrement of $a is $c \n";
+
